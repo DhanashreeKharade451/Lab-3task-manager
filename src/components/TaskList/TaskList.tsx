93c814df92d453afaebe46d321cd518022091935
Dhanashree <dhanashreeakharade@gmail.com>
2026-02-22 23:36:04 -0500

    Passes list props to ul

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,10 +1,21 @@
 import type { TaskListProps } from "../../types";
 import TaskItem from "../TaskItem/TaskItem.tsx";
-const TaskList = ({ tasks, onStatusChange, onDelete }: TaskListProps) => {
+
+//declare TaskList Function
+export const TaskList = ({ tasks, onStatusChange, onDelete }: TaskListProps) => {
+        
+    if(tasks.length === 0){
+        return <p className="p-4 text-gray-500">No tasks found.</p>
+    }
+
   return (
     <ul>
       {tasks.map((task) => (
-        <TaskItem task={task} />
+        <TaskItem key={task.id}
+        task={task}
+        onStatusChange={onStatusChange}
+        onDelete={onDelete}
+         />
       ))}
     </ul>
   );
